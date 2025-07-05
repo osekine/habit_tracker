b225eb2cb716c635cd23dc9d03cde97fe5339e19
NeonLaserUnicorn <osekine42@gmail.com>
2025-07-05 13:13:27 +0300

    Init commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const HabitTrackerApp());
+}
+
+class HabitTrackerApp extends StatelessWidget {
+  const HabitTrackerApp({super.key});
+
+  @override
+  Widget build(BuildContext context) => const MaterialApp(
+    home: CircularProgressIndicator(),
+  );
+}
