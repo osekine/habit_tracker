b76d668718bec4f8fcaca4565db3212d98d376cd
NeonLaserUnicorn <osekine42@gmail.com>
2025-07-05 21:14:16 +0300

    Change colors opacity

--- lib/theme/habit_colors.dart
+++ lib/theme/habit_colors.dart
@@ -5,145 +5,145 @@ import 'package:habit_tracker/theme/habit_color.dart';
 
 abstract final class HabitColors {
   static final red = HabitColor(
-    light: const Color(0xFFFFCCCC),
-    standart: const Color(0xFFFF8888),
+    light: const Color(0x44FFCCCC),
+    standart: const Color(0xAAFF8888),
     strong: const Color(0xFFFF4444),
   );
   static final orange = HabitColor(
-    light: const Color(0xFFF4D79F),
-    standart: const Color(0xFFD88A30),
+    light: const Color(0x44F4D79F),
+    standart: const Color(0xAAD88A30),
     strong: const Color(0xFFB25900),
   );
 
   static final yellow = HabitColor(
-    light: const Color(0xFFFFF49F),
-    standart: const Color(0xFFFFD700),
+    light: const Color(0x44FFF49F),
+    standart: const Color(0xAAFFD700),
     strong: const Color(0xFFFFB200),
   );
 
   static final green = HabitColor(
-    light: const Color(0xFFD7F49F),
-    standart: const Color(0xFF9ACD32),
+    light: const Color(0x44D7F49F),
+    standart: const Color(0xAA9ACD32),
     strong: const Color(0xFF76B041),
   );
 
   static final cyan = HabitColor(
-    light: const Color(0xFF9FEAFF),
-    standart: const Color(0xFF56BFFF),
+    light: const Color(0x449FEAFF),
+    standart: const Color(0xAA56BFFF),
     strong: const Color(0xFF00A1FF),
   );
 
   static final blue = HabitColor(
-    light: const Color(0xFFA5D8FF),
-    standart: const Color(0xFF409EFF),
+    light: const Color(0x44A5D8FF),
+    standart: const Color(0xAA409EFF),
     strong: const Color(0xFF1E60FF),
   );
 
   static final purple = HabitColor(
-    light: const Color(0xFFFA9FFF),
-    standart: const Color(0xFFC278FF),
+    light: const Color(0x44FA9FFF),
+    standart: const Color(0xAAC278FF),
     strong: const Color(0xFF8B3DAF),
   );
 
   static final pink = HabitColor(
-    light: const Color(0xFFFFA5F0),
-    standart: const Color(0xFFFF40FF),
+    light: const Color(0x44FFA5F0),
+    standart: const Color(0xAAFF40FF),
     strong: const Color(0xFFE01EFF),
   );
 
   static final gray = HabitColor(
-    light: const Color(0xFFECECEC),
-    standart: const Color(0xFFBDBDBD),
+    light: const Color(0x44ECECEC),
+    standart: const Color(0xAABDBDBD),
     strong: const Color(0xFF7F7F7F),
   );
 
   static final white = HabitColor(
-    light: const Color(0xFFFFFFFF),
-    standart: const Color(0xFFFFFFFF),
+    light: const Color(0x44FFFFFF),
+    standart: const Color(0xAAFFFFFF),
     strong: const Color(0xFFFFFFFF),
   );
 
   static final black = HabitColor(
-    light: const Color(0xFF333333),
-    standart: const Color(0xFF222222),
+    light: const Color(0x44333333),
+    standart: const Color(0xAA222222),
     strong: const Color(0xFF000000),
   );
 
   static final lemonYellow = HabitColor(
-    light: const Color(0xFFFFF49F),
-    standart: const Color(0xFFFFE066),
+    light: const Color(0x44FFF49F),
+    standart: const Color(0xAAFFE066),
     strong: const Color(0xFFFFCC00),
   );
 
   static final mint = HabitColor(
-    light: const Color(0xFF98FB98),
-    standart: const Color(0xFF76EE76),
+    light: const Color(0x4498FB98),
+    standart: const Color(0xAA76EE76),
     strong: const Color(0xFF52D052),
   );
 
   static final lavender = HabitColor(
-    light: const Color(0xFFFAE6FE),
-    standart: const Color(0xFFD6BCFA),
+    light: const Color(0x44FAE6FE),
+    standart: const Color(0xAAD6BCFA),
     strong: const Color(0xFFB583EE),
   );
 
   static final peach = HabitColor(
-    light: const Color(0xFFFCDCDC),
-    standart: const Color(0xFFFBB99A),
+    light: const Color(0x44FCDCDC),
+    standart: const Color(0xAAFBB99A),
     strong: const Color(0xFFF98F61),
   );
 
   static final sand = HabitColor(
-    light: const Color(0xFFF5F5DC),
-    standart: const Color(0xFFF0E68C),
+    light: const Color(0x44F5F5DC),
+    standart: const Color(0xAAF0E68C),
     strong: const Color(0xFFE5D774),
   );
 
   static final darkGreen = HabitColor(
-    light: const Color(0xFF8FBC8F),
-    standart: const Color(0xFF669966),
+    light: const Color(0x448FBC8F),
+    standart: const Color(0xAA669966),
     strong: const Color(0xFF4C764C),
   );
 
   static final turquoise = HabitColor(
-    light: const Color(0xFFA7FCE9),
-    standart: const Color(0xFF74DDE8),
+    light: const Color(0x44A7FCE9),
+    standart: const Color(0xAA74DDE8),
     strong: const Color(0xFF4ABDE8),
   );
 
   static final terracotta = HabitColor(
-    light: const Color(0xFFE4795C),
-    standart: const Color(0xFFC65345),
+    light: const Color(0x44E4795C),
+    standart: const Color(0xAAC65345),
     strong: const Color(0xFFA6372E),
   );
 
   static final fuchsia = HabitColor(
-    light: const Color(0xFFFFA7E9),
-    standart: const Color(0xFFFF66CC),
+    light: const Color(0x44FFA7E9),
+    standart: const Color(0xAAFF66CC),
     strong: const Color(0xFFE91E63),
   );
 
   static final lightGreen = HabitColor(
-    light: const Color(0xFFD0F0C0),
-    standart: const Color(0xFF90D090),
+    light: const Color(0x44D0F0C0),
+    standart: const Color(0xAA90D090),
     strong: const Color(0xFF54A054),
   );
 
   static final chocolate = HabitColor(
-    light: const Color(0xFF6F4E37),
-    standart: const Color(0xFF5D4037),
+    light: const Color(0x446F4E37),
+    standart: const Color(0xAA5D4037),
     strong: const Color(0xFF3E271E),
   );
 
   static final anthracite = HabitColor(
-    light: const Color(0xFF4D4D4D),
-    standart: const Color(0xFF333333),
+    light: const Color(0x444D4D4D),
+    standart: const Color(0xAA333333),
     strong: const Color(0xFF262626),
   );
 
   static final ivory = HabitColor(
-    light: const Color(0xFFFDF5E6),
-    standart: const Color(0xFFF5F0DC),
+    light: const Color(0x44FDF5E6),
+    standart: const Color(0xAAF5F0DC),
     strong: const Color(0xFFEBE5D9),
   );
 }
