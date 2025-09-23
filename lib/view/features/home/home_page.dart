e818f4b879eade4dffcf1c10a025ba2855278da4
NeonLaserUnicorn <osekine42@gmail.com>
2025-09-24 00:14:40 +0300

    opens calendar on tap

--- lib/view/features/home/home_page.dart
+++ lib/view/features/home/home_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:habit_tracker/navigation/router.dart';
 import 'package:habit_tracker/theme/habit_colors.dart';
+import 'package:habit_tracker/view/features/calendar/habit_calendar_page.dart';
 import 'package:habit_tracker/view/features/home/categories_header.dart';
 import 'package:habit_tracker/view/features/home/home_page_header.dart';
 import 'package:habit_tracker/view/features/home/year_habit_widget.dart';
@@ -25,23 +26,24 @@ class _HomePageState extends State<HomePage> {
   }
 
   @override
-  Widget build(BuildContext context) => ValueListenableBuilder(
-    valueListenable: widget.vm.habits,
-    builder:
-        (_, habits, __) => Scaffold(
-          backgroundColor: ColorCollection.monocrome['white']!.baseColor
-              .withAlpha(255),
-          floatingActionButton:
-              !kDebugMode
-                  ? null
-                  : FloatingActionButton(
-                    onPressed: () async {
-                      await widget.vm.archiveHabits();
-                      setState(() {});
-                    },
-                    child: const Icon(Icons.delete),
-                  ),
-          body: RefreshIndicator(
+  Widget build(BuildContext context) => Scaffold(
+    backgroundColor: ColorCollection.monocrome['white']!.baseColor.withAlpha(
+      255,
+    ),
+    floatingActionButton:
+        !kDebugMode
+            ? null
+            : FloatingActionButton(
+              onPressed: () async {
+                await widget.vm.archiveHabits();
+                setState(() {});
+              },
+              child: const Icon(Icons.delete),
+            ),
+    body: ValueListenableBuilder(
+      valueListenable: widget.vm.habits,
+      builder:
+          (_, habits, __) => RefreshIndicator(
             onRefresh: widget.vm.load,
             child: CustomScrollView(
               slivers: [
@@ -55,26 +57,31 @@ class _HomePageState extends State<HomePage> {
                 ),
                 SliverPersistentHeader(
                   pinned: true,
-                  delegate: CategoriesHeader(
-                    extent: 32,
-                    vm: widget.vm,
-                  ),
+                  delegate: CategoriesHeader(extent: 32, vm: widget.vm),
                 ),
                 SliverList(
-                  delegate: SliverChildListDelegate([
-                    Center(
+                  delegate: SliverChildBuilderDelegate((_, index) {
+                    final habit = habits[index];
+                    return Center(
                       child: Column(
                         children: [
-                          for (final habit in habits)
-                            YearHabitWidget(vm: habit),
+                          GestureDetector(
+                            onTap:
+                                () async =>
+                                    HabitCalendarPage.showAsModalBottomSheet(
+                                      context,
+                                      vm: habit,
+                                    ),
+                            child: YearHabitWidget(vm: habit),
+                          ),
                         ],
                       ),
-                    ),
-                  ]),
+                    );
+                  }, childCount: habits.length,),
                 ),
               ],
             ),
           ),
-        ),
+    ),
   );
 }
